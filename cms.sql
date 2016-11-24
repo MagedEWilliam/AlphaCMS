4dfc5a755996d452c0e017ecf32ebcd4166e028c
Maged E. William <Maged.EWilliam@Gmail.com>
2016-11-24 23:43:44 +0200

    updating database

--- cms.sql
+++ cms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2016 at 08:52 PM
+-- Generation Time: Nov 24, 2016 at 10:42 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -34,14 +34,15 @@ CREATE TABLE IF NOT EXISTS `category` (
   `image` text NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `Name` (`Name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`ID`, `Name`, `NameAr`, `NameCh`, `image`) VALUES
-(1, 'Electrical Boxes & Brackets', 'صناديق كهربائية وأقواس', '电气盒和支架', 'http://www.mrelectricdecatur.com/pictures/el_icon_4.jpg');
+(1, 'Electrical Boxes & Brackets', 'صناديق كهربائية وأقواس', '电气盒和支架', 'http://www.mrelectricdecatur.com/pictures/el_icon_4.jpg'),
+(2, 'Conduit', 'أنبوب', '导管', 'http://www.ionnic.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/c/action_conduit.jpg');
 
 -- --------------------------------------------------------
 
@@ -52,23 +53,59 @@ INSERT INTO `category` (`ID`, `Name`, `NameAr`, `NameCh`, `image`) VALUES
 CREATE TABLE IF NOT EXISTS `catproperty` (
   `categoryID` int(11) NOT NULL,
   `propertyID` int(11) NOT NULL,
-  `valueID` int(11) NOT NULL
+  `valueID` int(11) NOT NULL,
+  `filterbyme` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `catproperty`
 --
 
-INSERT INTO `catproperty` (`categoryID`, `propertyID`, `valueID`) VALUES
-(1, 2, 2),
-(1, 1, 1),
-(1, 3, 3),
-(0, 3, 0),
-(0, 2, 2),
-(0, 1, 1),
-(3, 3, 3),
-(3, 2, 2),
-(3, 1, 1);
+INSERT INTO `catproperty` (`categoryID`, `propertyID`, `valueID`, `filterbyme`) VALUES
+(1, 2, 2, 0),
+(1, 1, 1, 0),
+(1, 3, 3, 0),
+(0, 3, 0, 0),
+(0, 2, 2, 0),
+(0, 1, 1, 0),
+(3, 3, 3, 0),
+(3, 2, 2, 0),
+(3, 1, 1, 0),
+(4, 3, 3, 0),
+(4, 2, 2, 0),
+(0, 4, 1, 0),
+(0, 5, 1, 0),
+(0, 4, 1, 0),
+(0, 5, 1, 0),
+(0, 4, 1, 0),
+(0, 5, 1, 0),
+(0, 4, 1, 0),
+(0, 5, 1, 0),
+(5, 4, 1, 0),
+(5, 5, 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locale`
+--
+
+CREATE TABLE IF NOT EXISTS `locale` (
+  `key` varchar(250) NOT NULL,
+  `value` text CHARACTER SET utf8 NOT NULL,
+  `valueAr` text CHARACTER SET utf8 NOT NULL,
+  `valueCh` text CHARACTER SET utf8 NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`key`),
+  UNIQUE KEY `Key` (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `locale`
+--
+
+INSERT INTO `locale` (`key`, `value`, `valueAr`, `valueCh`, `deleted`) VALUES
+('login', 'Login', 'دخول', '注册', 0);
 
 -- --------------------------------------------------------
 
@@ -83,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `property` (
   `NameCh` varchar(250) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `Name` (`Name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `property`
@@ -92,7 +129,9 @@ CREATE TABLE IF NOT EXISTS `property` (
 INSERT INTO `property` (`ID`, `Name`, `NameAr`, `NameCh`) VALUES
 (1, 'Width', 'عرض', '宽度'),
 (2, 'Height', 'ارتفاع', '高度'),
-(3, 'Electrical Boxes & Brackets', 'صناديق كهربائية وأقواس', '电气盒和支架');
+(3, 'Electrical Boxes & Brackets', 'صناديق كهربائية وأقواس', '电气盒和支架'),
+(4, 'Diameter', 'قطر', '直径'),
+(5, 'Sub-Category', 'قائمة فرعية', '子分类');
 
 -- --------------------------------------------------------
 
@@ -109,15 +148,17 @@ CREATE TABLE IF NOT EXISTS `subcategory` (
   `NameCh` varchar(250) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `code` (`code`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `subcategory`
 --
 
 INSERT INTO `subcategory` (`ID`, `catID`, `code`, `Name`, `NameAr`, `NameCh`) VALUES
-(1, 1, '', 'Hard Shell Ceiling Box with Adjustable Bar Hanger', ' صندوق بقوقعة صلبة للسقف قابلة للتعديل شريط شماعات', '硬壳天花板盒带可调式酒吧衣架'),
-(3, 1, 'heyimaCode123', 'Fan Fixture Box', 'مروحة تركيبات صندوق', '范夹具盒');
+(1, 1, 'heyimaCode321', 'Hard Shell Ceiling Box with Adjustable Bar Hanger', ' صندوق بقوقعة صلبة للسقف قابلة للتعديل شريط شماعات', '硬壳天花板盒带可调式酒吧衣架'),
+(3, 1, 'heyimaCode123', 'Fan Fixture Box', 'مروحة تركيبات صندوق', '范夹具盒'),
+(4, 1, '1233', '32', '32', '23'),
+(5, 2, 'uniqueCode', 'EMT', 'EMT', 'EMT');
 
 -- --------------------------------------------------------
 
@@ -132,7 +173,7 @@ CREATE TABLE IF NOT EXISTS `value` (
   `valueAr` varchar(250) CHARACTER SET utf8 NOT NULL,
   `valueCh` varchar(250) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `value`
@@ -141,7 +182,9 @@ CREATE TABLE IF NOT EXISTS `value` (
 INSERT INTO `value` (`ID`, `propertyID`, `value`, `valueAr`, `valueCh`) VALUES
 (1, 1, '2.5 cm', '2.5 cm', '2.5 cm'),
 (2, 2, '2.5 cm', '2.5 cm', '2.5 cm'),
-(3, 3, 'Ceiling Box', 'صندوق السقف', '天花板盒');
+(3, 3, 'Ceiling Box', 'صندوق السقف', '天花板盒'),
+(4, 4, '1in', '1in', '1in'),
+(5, 5, 'Metallic', 'معدني', '金属的');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
